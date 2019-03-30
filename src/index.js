06a05e41c969c5e909cfd0c559a46accd123a212
adam <adam.sobiesak@gmail.com>
2019-03-30 16:10:23 +0100

    initial comment

--- src/index.js
+++ src/index.js
@@ -1,1 +1,1 @@
-importScripts ('https://github.com/daftcode/daftacademy-frontend_levelup-spring2019/blob/master/prace_domowe/lekcja_1/script.js')
+importscrpit ("https://github.com/daftcode/daftacademy-frontend_levelup-spring2019/blob/master/prace_domowe/lekcja_1/script.js")
